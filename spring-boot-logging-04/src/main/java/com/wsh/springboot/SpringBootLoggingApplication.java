0c053eee9173a64d7a68b2120a40d7fbb7f08686
wushanghui <wushanghui@aliyun.com>
2019-02-02 23:30:11 +0800

    spring-boot-logging-04 commit...

---
+++ spring-boot-logging-04/src/main/java/com/wsh/springboot/SpringBootLoggingApplication.java
@@ -0,0 +1,14 @@
+package com.wsh.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLoggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLoggingApplication.class, args);
+	}
+
+}
+
