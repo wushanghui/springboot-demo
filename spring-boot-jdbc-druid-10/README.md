e74aab456bcb8198a79ad93c61a70b9cf138f1e8
wshcwj <wush93@aliyun.com>
2019-02-23 15:58:56 +0800

    spring boot jdbc durid commit ..

---
+++ spring-boot-jdbc-druid-10/README.md
@@ -0,0 +1,4 @@
+Druid数据源管理后台的访问地址
+http://localhost:8080/druid/login.html
+
+使用DruidConfig 配置的用户和密码登录
---
+++ spring-boot-jdbc-druid-10/src/main/java/com/wsh/springboot/SpringBootJdbcDruidApplication.java
@@ -0,0 +1,14 @@
+package com.wsh.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJdbcDruidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJdbcDruidApplication.class, args);
+    }
+
+}
+
