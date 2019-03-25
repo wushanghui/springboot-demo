736f13791cac894e3797e7fdfc5e280ccba629f7
wshcwj <wush93@aliyun.com>
2019-03-25 21:46:57 +0800

    security

---
+++ spring-boot-security-18/src/main/java/com/wsh/springboot/SpringBootSecurity18Application.java
@@ -0,0 +1,13 @@
+package com.wsh.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurity18Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSecurity18Application.class, args);
+    }
+
+}
