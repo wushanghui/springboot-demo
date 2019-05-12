0ebdaf2178600ff9139f9455846faf4940363df6
wshcwj <wush93@aliyun.com>
2019-05-12 23:29:22 +0800

    actuator

---
+++ spring-boot-actuator-20/src/main/java/com/wsh/springboot/SpringBootActuator20Application.java
@@ -0,0 +1,19 @@
+package com.wsh.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 自定义健康状态指示器
+ * 1、编写一个指示器 实现 HealthIndicator 接口
+ * 2、指示器的名字 xxxxHealthIndicator
+ * 3、加入容器中
+ */
+@SpringBootApplication
+public class SpringBootActuator20Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootActuator20Application.class, args);
+    }
+
+}
